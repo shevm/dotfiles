1a3e1ac387b3db70a0a204461ff815248780cf11
leanile <admin@leanile.ch>
2019-12-17 12:11:29 +0200

    added code to copy abbreviations file to correct folder

--- vim-setup.py
+++ vim-setup.py
@@ -3,19 +3,26 @@ import shutil
 import sys
 from pathlib import Path
 
-src = Path('vimrc')
+HOME = Path().home()
+VIM_HOME = Path().home()/'.vim'
+vimrc_src = Path('vimrc')
 
 if sys.platform == 'darwin':
     char = '.'
 elif sys.platform == 'win32':
     char = '_'
 
-dst = Path().home()/(char+str(src.name))
+# symlink vimrc file
+vimrc_dst = HOME/(char+str(vimrc_src.name))
+if not vimrc_dst.exists():
+    vimrc_dst.symlink_to(vimrc_src.absolute())
 
-
-dst.symlink_to(src.absolute())
-
+# move abbreviations file to the correct folder
+abbr_src = Path('abbr.vim')
+abbr_dst = VIM_HOME/'abbr'/abbr_src.name
+if not abbr_dst.exists():
+    abbr_dst.mkdir(parents=True)
+    abbr_dst.symlink_to(abbr_src.absolute())
 
 print('Done!')
-print(f'Link created at {dst}')
 
